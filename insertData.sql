d84ded17b072828e0507b3e8da2440df29c0ce8b
Zhang Zeng (Jennie) <zhangzeng9730@gmail.com>
2018-10-28 15:13:29 +0800

    Create insertData.sql

---
+++ insertData.sql
@@ -0,0 +1,23 @@
+use f34ee;
+
+insert into products values
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_1.jpg'),
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_2.jpg'),
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_3.jpg'),
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_4.jpg'),
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_5.jpg'),
+    (NULL, 'female', 1, 0, 888.88, 'new femail shoes', 'women_6.jpg'),
+    (NULL, 'female', 1, 0, 988.88, 'new femail shoes', 'women_7.jpg'),
+    (NULL, 'female', 1, 0, 988.88, 'new femail shoes', 'women_8.jpg'),
+    (NULL, 'female', 1, 0, 988.88, 'new femail shoes', 'women_9.jpg'),
+    (NULL, 'female', 1, 0, 988.88, 'new femail shoes', 'women_10.jpg'),
+    (NULL, 'female', 1, 0, 988.88, 'new femail shoes', 'women_11.jpg'),
+    (NULL, 'female', 1, 0, 1088.88, 'new femail shoes', 'women_12.jpg'),
+    (NULL, 'female', 1, 0, 1088.88, 'new femail shoes', 'women_13.jpg'),
+    (NULL, 'female', 1, 0, 1088.88, 'new femail shoes', 'women_14.jpg');
+
+insert into orders values
+    (NULL, 1, 3, 888.88, '2018-10-26');
+
+insert into users values
+    (NULL, 'zzhang034', '123456');
