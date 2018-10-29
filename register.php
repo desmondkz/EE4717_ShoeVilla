2cc97f79dcd262f74f1514739d481137d3ef078d
desmondkz <39067134+desmondkz@users.noreply.github.com>
2018-10-29 12:10:43 +0800

    Create register.php
    php file for user registration (incomplete for SQL Connection)

---
+++ register.php
@@ -0,0 +1,52 @@
+<?php 
+include('mySQLConnect.php');
+?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ShoeVilla | User Registration</title>
+    <link rel="stylesheet" href="css/register.css" type="text/css">
+</head>
+
+
+
+<body>
+    <div class="header">
+        <h2>Register</h2>
+    </div>
+
+    <form method="post" action="register.php">
+        <!-- Display validation errors here -->
+        <?php 
+        include('errors.php');
+        ?>
+
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="text" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="text" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm Password</label>
+            <input type="text" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="register" class="btn">Register</button>
+        </div>
+        <p>
+            Already a member?   <a href="login.php">Sign in</a>
+        </p>
+
+    </form>
+</body>
+</html>
