b94ae151023644728e0fab27a5ce0b2b77538a4e
desmondkz <39067134+desmondkz@users.noreply.github.com>
2018-11-04 00:25:55 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,41 @@
+<?php 
+    session_start();
+
+    // store to test if they were logged in
+    $old_user = $_SESSION['username'];
+    unset($_SESSION['username']);
+    session_destroy();
+?>
+
+<html>
+<head>
+    <title>ShoeVilla | Logout</title>
+    <link rel="stylesheet" href="css/register.css" type="text/css">
+    <link rel="stylesheet" href="css/index_style.css" type="text/css">
+    <!--Website Tab Icon-->
+    <link rel="shortcut icon" href="ShoeVillaBanner.ico">
+</head>
+
+<center>
+    <body>
+        <div class="wrapper">
+            <div class="subheader">
+                <p class="text" align="center">FREE INTERNATIONAL STANDARD SHIPPING ON ORDERS OF $200+</p>
+            </div>
+            
+            <h1>Log out</h1>
+
+            <?php 
+                if (!empty($old_user)) {
+                    echo 'You are now logged out. <br />';
+                }
+                else {
+                    // if they weren't logged in but came to this page somehow
+                    echo 'You were not logged in, so please log in first.';
+                }
+            ?>
+            <a href="index.php">Return to main page.</a>
+        </div>
+    </body>
+</center>
+</html>
