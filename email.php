6669bf56d5debfc14ab772af0b27264821fb8131
desmondkz <39067134+desmondkz@users.noreply.github.com>
2018-11-05 14:01:50 +0800

    Update email.php

--- email.php
+++ email.php
@@ -1,41 +1,22 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+include 'mySQL.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';                 // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'desmondkz1110@gmail.com';          // SMTP username
-    $mail->Password = 'Desmondkz5683,.';                  // SMTP password
-    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
+var_dump($_SESSION['username']);
 
-    //Recipients
-    $mail->setFrom('Auto-generated@gmail.com', 'ShoeVilla Customer Service');
-    $mail->addAddress('KANI0011@e.ntu.edu.sg');           // Add a recipient
+$to      = 'f34ee@localhost';
+$subject = 'ShoeVilla Order confirmation';
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');       // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  // Optional name
+$message = 'Hi Customer, thanks'
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Purchase Confirmation';
-    $mail->Body    = 'Hi';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
+$headers = 'From: f34ee@localhost' . "\r\n" .
+    'Reply-To: f34ee@localhost' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-?>
+mail($to, $subject, $message, $headers,'-ff34ee@localhost');
+echo ("mail sent to : ".$to);
+?> 
+
+</>
+</html>
