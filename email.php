4a6109bc8b1776adb8d58eb559e08fc5ba21fa78
desmondkz <39067134+desmondkz@users.noreply.github.com>
2018-11-05 15:19:47 +0800

    Update email.php

--- email.php
+++ email.php
@@ -1,22 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-
-include 'mySQL.php';
-
-var_dump($_SESSION['username']);
-
 $to      = 'f34ee@localhost';
-$subject = 'ShoeVilla Order confirmation';
+$subject = 'ShoeVilla Order Confirmation';
 
-$message = 'Hi Customer, thanks'
-
+$message = 'Hi, thanks for shopping at ShoeVilla! This email is to confirm your purchase.';
 
 $headers = 'From: f34ee@localhost' . "\r\n" .
     'Reply-To: f34ee@localhost' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers,'-ff34ee@localhost');
-echo ("mail sent to : ".$to);
 ?> 
 
-</>
+</body>
 </html>
